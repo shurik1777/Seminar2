d65cb8b86398740b2b7aaf6e20807733413cc5c6
Wosmerik Alexandr <shurik1777@gmail.com>
2024-05-16 20:36:14 +0300

    first commit add 02 seminar

---
+++ task_02.py
@@ -0,0 +1,39 @@
+"""
+Задание №2.
+Создать страницу, на которой будет изображение и ссылка
+на другую страницу, на которой будет отображаться форма
+для загрузки изображений.
+"""
+from flask import Flask, render_template, request
+from pathlib import PurePath, Path  # для работы с файлами
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index/')
+def index():
+    return render_template('index.html', title='Стартовая страница')
+
+
+@app.route('/data/')
+def data():
+    return render_template('data.html', title='Информация')
+
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload_page():
+    if request.method == 'POST':
+        file = request.files.get('file')
+        file_name = secure_filename(file.filename)
+        file.save(PurePath.joinpath(Path.cwd(), 'static/image',
+                                    file_name))
+        # Уточнение если не будет создана директория uploads
+        # - то будут ошибки не найденных путей
+        return f"Файл {file_name} загружен на сервер"
+    return render_template('upload.html', title='Получаем данные')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
