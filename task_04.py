d8d11a10edd8956ff3b8e38962b99f58dd0c032a
Wosmerik Alexandr <shurik1777@gmail.com>
2024-05-19 09:45:10 +0300

    add task 3 end project seminar 2

--- task_04.py
+++ task_04.py
@@ -5,16 +5,37 @@
 При нажатии кнопки будет произведен подсчет количества слов
 в тексте и переход на страницу с результатом.
 """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index/')
+@app.route('/index')
 def index():
-    return render_template('index_0.html',
+    return render_template('index_7.html',
                            title='Стартовая страница')
+
+
+@app.route('/data')
+def data():
+    return render_template('data.html',
+                           title='Информация')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    text = request.form.get('text')
+    if text:
+        words = text.split()
+        count = len(words)
+        return redirect(url_for('result', count=count))
+    return redirect(url_for('index'))
+
+
+@app.route('/result/<count>')
+def result(count):
+    return render_template('result_count.html', count=count)
 
 
 if __name__ == '__main__':
